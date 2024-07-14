db0065b11dc982d26221f2f42cc6f480fd49b4c2
Mohamed Affan <mohamedaffan477@gmail.com>
2024-07-14 08:39:57 +0000

    SpaceName

---
+++ screener/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import SpaceName from './components/SpaceName'
+
+function App() {
+  return (
+    <div id='app'>
+      <SpaceName />
+    </div>
+  )
+}
+
+export default App
---
+++ screener/src/components/SpaceName.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function SpaceName() {
+  const [userInput, setUserInput] = useState('');
+
+  // Load stored data from local storage when the component mounts
+  useEffect(() => {
+    const storedInput = localStorage.getItem('userInput');
+    if (storedInput) {
+      setUserInput(storedInput);
+    }
+  }, []);
+
+  // Update local storage whenever userInput changes
+  useEffect(() => {
+    localStorage.setItem('userInput', userInput);
+  }, [userInput]);
+
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  return (
+    <div className="SpaceName">
+      <textarea
+        className="editable-div"
+        value={userInput}
+        onChange={handleInputChange}
+        placeholder="Space's Name"
+        maxLength={20}
+      />
+    </div>
+  );
+}
+
+
+export default SpaceName;
